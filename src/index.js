969fbf91b47467475ce631970e7e402036c8c7b8
cdanmar <cdanmar@gmail.com>
2017-12-03 16:23:44 -0600

    testing the DOM

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create new component, this compontent should produce some html
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this generated html and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
