863dc9230d1eaac901413caac122135d42582f93
cdanmar <cdanmar@gmail.com>
2017-12-09 22:40:30 -0600

    able to click on video sidebar, added some styling

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-import VideoDetial from './components/video_detail'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyDq5lg4TYrJs-zh3txMyFSRWRqqSpEEH50';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch(
       {key: API_KEY, term: 'basset hounds'}, (videos) => {
-        this.setState({ videos });
+        this.setState({
+           videos: videos,
+           selectedVideo: videos[0]
+          });
       }
     );
   }
@@ -23,8 +29,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetial video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
